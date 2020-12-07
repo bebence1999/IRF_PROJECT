477adcd81febae31880cc375dae6d504380d7a4a
Bence-Mr.Chatbee <Bence-Mr.Chatbee@DESKTOP-HDGHG3Q>
2020-12-07 22:43:07 +0100

    Login form kész

---
+++ Kórházi betegkezelő_XAICMP/Kórházi betegkezelő_XAICMP/Login.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Kórházi_betegkezelő_XAICMP
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.txtFelhasználónév = new System.Windows.Forms.TextBox();
+            this.txtJelszó = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFelhasználónév
+            // 
+            this.txtFelhasználónév.Location = new System.Drawing.Point(774, 39);
+            this.txtFelhasználónév.Name = "txtFelhasználónév";
+            this.txtFelhasználónév.Size = new System.Drawing.Size(100, 20);
+            this.txtFelhasználónév.TabIndex = 0;
+            // 
+            // txtJelszó
+            // 
+            this.txtJelszó.Location = new System.Drawing.Point(774, 88);
+            this.txtJelszó.Name = "txtJelszó";
+            this.txtJelszó.PasswordChar = '*';
+            this.txtJelszó.Size = new System.Drawing.Size(100, 20);
+            this.txtJelszó.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(684, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Felhasználónév:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(719, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Jelszó:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLogin.Location = new System.Drawing.Point(774, 133);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(100, 37);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Bejelentkezés";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1030, 652);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtJelszó);
+            this.Controls.Add(this.txtFelhasználónév);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFelhasználónév;
+        private System.Windows.Forms.TextBox txtJelszó;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
